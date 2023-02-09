b8418c1b3a5759cddf688ff1fff753fdf1a37d95
ajinkyajoshi <ajinkyaj238@gmail.com>
2023-02-08 23:51:41 -0500

    updated comments

--- SmartClient.h
+++ SmartClient.h
@@ -18,15 +18,58 @@ using namespace std;
 
 class SmartClient
 {
+
+/*
+ * Private data members of the smartclient class include the predetermined range and size values of the vectors.
+ */
 private:
 	int m = 10, n = 5;
 	LockBox* lock;
 public:
+    /* Constructors method of Smartclient initializes the range and size values to 5 and 10. And have them equal to the
+     * length and range.
+     * @pre none
+     * @post created objects based on the constructors.
+     * @param range and length integers.
+     * @return object
+    */
 	SmartClient(int range, int length);
 	SmartClient();
+
+
+    /* Creates new lockbox object.
+     * @pre none
+     * @post new lockbox object created
+     * @param guess vector
+     * @return lockbox object.
+    */
 	LockBox clientGuess(vector<int> guess1);
+
+
+    /*creates client feedback object.
+     * @pre none
+     * @post client feedback object and sets the correct and incorrect integers of client feedback
+     * @param lockbox object
+     * @return clientfeedback object.
+    */
 	ClientFeedback getFeedback(LockBox* guess);
+
+
+    /* checks if lockbox can be opened.
+     * @pre none
+     * @post none
+     * @param clientfeedback pointer object.
+     * @return true or false.
+    */
 	bool isOpened(ClientFeedback* obj);
+
+
+    /* opens the lockbox with a cout
+     * @pre none
+     * @post cout displayed on terminal
+     * @param none
+     * @return none.
+    */
 	void openLockBox();
 };
 
